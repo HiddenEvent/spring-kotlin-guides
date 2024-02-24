0d3ac207a1fc54f798d26233f6845ccec42f8fba
RichardKim <hiddeneventor@gmail.com>
2024-02-24 23:10:55 +0900

    spring cloud 의존 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
     kotlin("jvm")
     kotlin("plugin.spring") apply false
     id("org.springframework.boot") apply false
-    id("io.spring.dependency-management") apply false
+    id("io.spring.dependency-management")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 val projectGroup: String by project
 val applicationVersion: String by project
-
+val springCloudVersion: String by project
 configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
@@ -35,10 +35,10 @@ subprojects {
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
 
-
     dependencies {
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation(platform("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}"))
         annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
